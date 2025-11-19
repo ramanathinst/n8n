aec54c9345d97b1b55dcaed98f219c3db1bb7228
Ramanath <ramanathinst@gmail.com>
2025-11-19 16:34:15 +0530

    17: Editor-state

--- src/features/executions/components/base-execution-node.tsx
+++ src/features/executions/components/base-execution-node.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { type NodeProps, Position } from "@xyflow/react";
+import { type NodeProps, Position, useReactFlow } from "@xyflow/react";
 import type {LucideIcon } from "lucide-react";
 import Image from "next/image";
 import { memo, type ReactNode, useCallback } from "react";
 import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
 import { BaseHandle } from "@/components/react-flow/base-handle";
 import { WorkflowNode } from "@/components/workflow-node";
+import { type NodeStatus, NodeStatusIndicator } from "@/components/react-flow/node-status-indicator";
 
 
 interface BaseExecutionNodeProps extends NodeProps {
@@ -14,23 +15,37 @@ interface BaseExecutionNodeProps extends NodeProps {
     name: string;
     description?: string;
     children?: React.ReactNode;
-    // status?: Nodestatus
+    status?: NodeStatus;
     onSettings?: () => void;
     onDubleClick?: () => void;
 }
 
 export const BaseExecutionNode = memo(({
+    id,
     icon: Icon,
     name,
     description,
     children,
+    status = "initial",
     onSettings,
     onDubleClick
 }: BaseExecutionNodeProps) => {
 
-    const handleDelete = () => {
-
-    }
+    const { setNodes, setEdges } = useReactFlow();
+        const handleDelete = () => {
+            setNodes((currentNodes) => {
+                const updatedNodes = currentNodes.filter((node) => node.id !== id);
+                return updatedNodes;
+            });
+    
+            // 2. Remove all edges connected to that node
+            setEdges((currentEdges) => {
+                const updatedEdges = currentEdges.filter(
+                    (edge) => edge.source !== id && edge.target !== id
+                );
+                return updatedEdges;
+            });
+        };
     return(
         <WorkflowNode
             name={name}
@@ -38,27 +53,32 @@ export const BaseExecutionNode = memo(({
             onDelete={handleDelete}
             onSetting={onSettings}
         >
-            <BaseNode onDoubleClick={onDubleClick}>
-                <BaseNodeContent>
-                    { typeof Icon === "string" ? (
-                        <Image src={Icon} width={40} height={40} className="size-4" alt={name} />
-                    ): (
-                        <Icon className="text-muted-foreground size-4" /> 
-                    )}
-                        {children}
-                        <BaseHandle 
-                            id="target-1"
-                            type="target"
-                            position={Position.Left}
-                        />
+            <NodeStatusIndicator 
+                status={status}
+                variant="border"
+            >
+                <BaseNode status={status} onDoubleClick={onDubleClick}>
+                    <BaseNodeContent>
+                        { typeof Icon === "string" ? (
+                            <Image src={Icon} width={40} height={40} className="size-4" alt={name} />
+                        ): (
+                            <Icon className="text-muted-foreground size-4" /> 
+                        )}
+                            {children}
+                            <BaseHandle 
+                                id="target-1"
+                                type="target"
+                                position={Position.Left}
+                            />
 
-                        <BaseHandle 
-                            id="source-1"
-                            type="source"
-                            position={Position.Right}
-                        />
-                </BaseNodeContent>
-            </BaseNode>
+                            <BaseHandle 
+                                id="source-1"
+                                type="source"
+                                position={Position.Right}
+                            />
+                    </BaseNodeContent>
+                </BaseNode>
+            </NodeStatusIndicator>
         </WorkflowNode>
     )
 })
--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import type { Node, NodeProps } from "@xyflow/react"
+import { useReactFlow, type Node, type NodeProps } from "@xyflow/react"
 import { GlobeIcon } from "lucide-react"
-import { memo } from "react"
+import { memo, useState } from "react"
 import { BaseExecutionNode } from "../base-execution-node"
+import { FormValues, HttpRequestDialog } from "./dialog"
 
 type HttpRequestNodeData = {
     endpoint?: string;
@@ -15,20 +16,49 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+    const [dialogOpen, setDialogOpen ] = useState(false);
+    const handleOpenSettings = () => setDialogOpen(true)
+    const nodeStatus = "initial"
+    const { setNodes} = useReactFlow();
 
-    const nodeData = props.data as HttpRequestNodeData;
+    const handleSubmit = (values: FormValues) => {
+        setNodes((nodes) => nodes.map((node) => {
+            if (node.id === props.id) {
+                return {
+                    ...node,
+                    data: {
+                        ...node.data,
+                        endpoint: values.endpoint,
+                        method: values.method,
+                        body: values.body,
+                    },
+                };
+            }
+            return node;
+        }));
+    }
+
+    const nodeData = props.data;
     const description = nodeData?.endpoint ? `${nodeData.method || "GET"}: ${nodeData.endpoint}` : "Not configured"
-
     return(
         <>
+            <HttpRequestDialog 
+                open={dialogOpen} 
+                onOpenChange={setDialogOpen} 
+                onSubmit={handleSubmit}
+                defaultEndpoint={nodeData.endpoint}
+                defaultBody={nodeData.body}
+                defaultMethod={nodeData.method}
+            />
             <BaseExecutionNode 
                 {...props}
                 id={props.id}
                 icon={GlobeIcon}
                 name="HTTP Request"
+                status={nodeStatus}
                 description={description}
-                onSettings={() => {}}
-                onDubleClick={() => {}}
+                onSettings={handleOpenSettings}
+                onDubleClick={handleOpenSettings}
             />
         </>
     )
