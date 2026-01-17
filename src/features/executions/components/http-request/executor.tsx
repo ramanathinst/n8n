9a69991dc448c1fd0c9e230f819d2431a5e576ce
Ramanath <ramanathinst@gmail.com>
2026-01-17 12:59:28 +0530

    20: ndoe templating

--- src/features/executions/components/http-request/executor.tsx
+++ src/features/executions/components/http-request/executor.tsx
@@ -1,11 +1,17 @@
+import Handlebars from "handlebars"
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 
+Handlebars.registerHelper("json", (context) => {
+    const jsonString = JSON.stringify(context, null, 2);
+    const safeString = new Handlebars.SafeString(jsonString);
+    return safeString;
+});
 type HttpRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
 };
 
@@ -18,26 +24,31 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     // TODO: Publish "loading" state for http request
 
     if (!data.endpoint) {
-        // TODO: Publish "error" state for http request
         throw new NonRetriableError(
             "HTTP Request node: No endpoint configured",
         );
     }
     if (!data.variableName) {
-        // TODO: Publish "error" state for http request
         throw new NonRetriableError(
-            "Variable name not configured",
+            "HTTP Request node: Variable name not configured",
+        );
+    }
+    if (!data.method) {
+        throw new NonRetriableError(
+            "HTTP Request node: No Method configured",
         );
     }
 
     const result = await step.run("http-request", async () => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        const endpoint = Handlebars.compile(data.endpoint)(context)
+        const method = data.method;
 
         const options: KyOptions = { method };
 
         if (["POST", "PUT", "PATCH"].includes(method)) {
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context)
+            JSON.parse(resolved)
+            options.body = resolved;
             options.headers = {
                 "Content-Type": "application/json"
             }
@@ -56,15 +67,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
                 data: responseData,
             },
         }
-        if (data.variableName) {
-            return {
-                ...context,
-                [data.variableName]: responsePayload
-            }
-        }
         return {
             ...context,
-            ...responsePayload
+            [data.variableName]: responsePayload
         }
     });
     // TODO: Publish "success" state for http request
