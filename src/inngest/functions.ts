b9bbe642c9617034d48d07057a8f1b7b996685cd
Ramanath <ramanathinst@gmail.com>
2026-02-05 07:46:23 +0530

    development

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -16,7 +16,7 @@ import { discordChannel } from "./channels/discord";
 export const executeWorkflow = inngest.createFunction(
   {
     id: "execute-workflow",
-    retries: 0, // REMOVE IN PRODUCTION,
+    retries: process.env.NODE_ENV === "production" ? 3 : 0,
     onFailure: async({ event, step}) => {
       return prisma.execution.update({
         where: { inngestEventId: event.data.event.id},
